dc8aff10fe22f9dfb16ced4418b61558225736dc
Swapnil Vidhate <swapnilvidhate@Swapnils-MacBook-Pro.local>
2025-04-22 20:41:55 +0530

    First Commit

---
+++ test/specs/mytest.e2e.js
@@ -0,0 +1,13 @@
+
+
+//all test cases are wrapped inside describe 
+describe('My Login application', () => { //basically like name of the test suite
+    it('should login with valid credentials', async () => { //these are specific test cases in the test suite
+       
+    })
+
+    it('should not login with invalid credentials', async () => {
+        
+    })
+})
+
