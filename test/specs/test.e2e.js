7a03c54ce4fb29cb8dd351d528095c835d4f98df
Swapnil Vidhate <swapnilvidhate@Swapnils-MacBook-Pro.local>
2025-04-28 14:22:52 +0530

    Swapnil | normal test cases for login

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,16 +1,37 @@
-import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+describe("Ecommerse Application", function()   //in latest JS models you don't need to write the function() keyword
+{
+    it("Login fail Page", async ()=>                 //instead you can use this fat operator               
+    {
+        //webdriverio Async i.e. there is no garuntee that it will executed line by line
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+        //await browser.implicitWait(5000)
+        await browser.pause(5000)
+        
+        console.log(await browser.getTitle())
+        await expect(browser).toHaveTitle(expect.stringContaining("Rahul Shetty Academy"))
+        $("input[name='username]").setValue("rahulshettyacademy")
+        //interesting thing is that if user to enter again in the same text box it will clear the previous string and enter again
+        await $("#username").setValue("swapnil")
+        await $("#password").setValue("newpassword")
+        await $("#terms").click()
+        await $("#signInBtn").click()
+        console.log(await $(".alert-danger").getText())
+        await browser.pause(5000)
+        console.log("***********************************************************")
+        await browser.pause(5000)
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
-            expect.stringContaining('You logged into a secure area!'))
-        await expect(SecurePage.flashAlert).toMatchElementSnapshot('flashAlert')
-    })
-})
+
+
+    } )
+
+
+
+
+
+
+
+}
+
+)
 
